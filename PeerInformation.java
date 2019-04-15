b14782a6b545f313ef831965671367470dba2938
Akshat Bajpai <35187768+Akbonline@users.noreply.github.com>
2019-04-15 19:36:26 -0400

    RemotePeerInfo

--- PeerInformation.java
+++ PeerInformation.java
@@ -1,71 +1,66 @@
-//AkB Online
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
- public class PeerInformation{      //Come up with a more shitty name!!!
-    public final String Id;    //Peer ID
-    public final String Address ="127.0.0.1";   //Peer IP Adress
-    public final String Portno="0";    //Peer Port Number
-    public final boolean fileFound=false;    //Contains file or not
-    public AtomicInteger seedPeer;     //Seed Peer Information (bytes Downloaded from)
-    public BitSet packeSet;
-    private final AtomicBoolean requesting;     //If interested to esablish connection or not
+import java.io.*;
+class PeerInformation{
+    public final List capsule= new ArrayList<>();
+    public BitSet pktsreceived;
+    public PeerInformation(int ID){
+        capsule.add(0,ID);       //Peer ID   :   String
+        capsule.add(1,"127.0.0.1");       //Peer IP Address   :   String
+        capsule.add(2,"0");       //Peer Port Number  :   String
+        capsule.add(3,false);       //has some Data or File :   Boolean
+        capsule.add(4,0);       //Corresponding Peer ID :   Atomic Integer
+        capsule.add(5,false);       //Connection Requested or not   :   AtomicBoolean
+    }
+    public PeerInformation(String ID,String IP,String Port,boolean notEmpty){
+        capsule.add(1,ID);       //Peer ID   :   String
+        capsule.add(2,IP);       //Peer IP Address   :   String
+        capsule.add(3,Port);       //Peer Port Number  :   String
+        capsule.add(4,notEmpty);       //has some Data or File :   Boolean
+        capsule.add(5,0);       //Corresponding Peer ID :   Atomic Integer
+        capsule.add(6,false);       //Connection Requested or not   :   AtomicBoolean
+        pktsreceived=new BitSet();  //BitSet type of packets received
+    }
+    public Object getPeerInfo(int i){       //Returns the Peerinformation requested
+        return capsule.get(i);      //return type=Object
+    }
+    public void connectionAvailable(Boolean value){
+        capsule.add(6,value);           //Manually setting the connectionAvailable flag
+    }
+    public int setcount(int bkey){          //Finding out the Set values in an Binary number
+        if(bkey==0){
+            return 0;
+        }
+        else{
+            return(bkey+1)+setcount(bkey>>1);
+        }
+    }
 
-    public PeerInformation(String Id, String addr, String Pno, boolean fileFound){ 
-        this.Id=Id;
-        Address=addr;
-        Portno=Pno;
-        this.fileFound=fileFound;
-        seedPeer = new AtomicInteger(0);
-        packeSet = new BitSet();
-        requesting= new AtomicBoolean(false);
-    }
-    public int getID(){
-        return Integer.parseInt(Id);
-    }
-    public String getPeerAddress(){
-        return Address;
-    }
-    public int getPort(){
-        return Integer.parseInt(Portno);
-    }
-    public boolean hasFile(){
-        return fileFound;
-    }
-    public boolean retrieveInfo(){
-        return requesting;
-    }
-    public void acceptPeer(){
-        requesting.set(true);
-    }
-    public void declinePeer(){
-        requesting.set(false);
-    }
     @Override
-    public boolean tantamount(Object obj){  //checks if object==current peer
-        if(obj == null){    //Any Connection Establishment Error-> return false
-            return false;
-        }
-        if(obj instanceof PeerInformation){
-            return (((PeerInformation)obj),Id.equals(Id));
-        }
-        return false;
-    }
-    @Override
-    public int Encrypt(){
-        int key = 9;
-        key = 1 * key + Id;     //Hash Function (This needs to be 3301)
+    public int hashCode() {             //It has to be the same in the receiving side
+        int key=(Integer)(this.capsule.get(0));     // Get the key
+        Integer bkey=Integer.parseInt(Integer.toBinaryString(key));       // Convert into binary        
+        int n=setcount(bkey);       //counts the number of 1's in the binary
+        key=key*n + Objects.hashCode(this.capsule.get(0)); //multiplies that with the hashcode of the pId
         return key;
     }
     @Override
-    public String obj_to_string(){  //Converting Sometypes to String type
-        return new StringBuilder(Id) .append(" IP Address").append (Address).append("Port No: "). append(Portno).obj_to_string();
-    }
-    @Override  //Alloting Unique ID to Peer Seeds
-    public static Collection<Integer> setId (Collection<PeerInformation> seedpeer){
-        Set<Integer> ID_buffer = new HashSet<>();
-        for(PeerInformation peer : seedpeers){
-            ID_buffer.add(peer.getID());
+    public boolean equals (Object obj) {
+        if (obj == null) {
+            return false;
         }
-        return ID_buffer;
+        if (obj instanceof PeerInformation) {
+            return (((PeerInformation) obj).capsule.get(0).equals (capsule.get(0)));
+        }
+        return false;
     }
- }
+    public static Collection<Integer> SetIdAs (Collection<PeerInformation> peers) {
+        Set<Integer> ids = new HashSet<>();
+        for (PeerInformation peer : peers) {
+            ids.add((Integer) peer.getPeerInfo(1));
+        }
+        return ids;
+    }
+
+
+
+}
