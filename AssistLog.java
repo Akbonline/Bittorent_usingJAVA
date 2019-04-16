b8da4810b8dbd5eadae495e2ebd371ec8d6065f6
Akshat Bajpai <35187768+Akbonline@users.noreply.github.com>
2019-04-16 04:36:39 -0400

    Add files via upload

--- AssistLog.java
+++ AssistLog.java
@@ -1,11 +1,10 @@
+package edu.ufl.cise.cnt5106c.log.modified;
+
+import edu.ufl.cise.cnt5106c.conf.PeerInformation;
 import java.io.*;
 import java.util.*;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+
+
 
 public class AssistLog{
     private Logger SessionID;
@@ -21,7 +20,7 @@ public class AssistLog{
             else{
                 peer1AsString.append(", ");      
             }
-            peer1AsString.append(peer.getPeerInfo(0));     //Converts the Peer ID as Integer
+            peer1AsString.append(peer.getPeerId());     //Converts the Peer ID as Integer
         }
         check=1;
         for(Integer ID:pID){
@@ -63,7 +62,7 @@ public class AssistLog{
             LogManager.getLogManager().readConfiguration(in);
         }
         catch (IOException e) {
-            //System.err.println(AssistLog.stackTraceToString(e));
+
             System.exit(1);
         }
         finally {
@@ -74,19 +73,9 @@ public class AssistLog{
             }
         }
     }
-    public synchronized void conf (String input) {  //log entry for configuration
-        SessionID.log(Level.CONFIG, input);
-    }
-    public synchronized void debug (String input) { //log entry for debug
-        SessionID.log(Level.FINE, input);
-    }
     public synchronized void info (String input) {  //log entry for fetching info
         SessionID.log (Level.INFO, input);
     }
-    public synchronized void severe (String input) {    //Critical Hit Point 
-        SessionID.log(Level.SEVERE, input);
-    }
-    public synchronized void warning (String input) {   
-        SessionID.log(Level.WARNING, input);
-    }
+    
+    
 }
